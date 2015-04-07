cf425d49c2644ffd2290aeb2f2f8f4f9c0ed423c
Lily <lilyc5459@gmail.com>
2015-04-06 20:29:49 -0400

    fixed header margins

---
+++ wiki.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+  // Wiki logo link
+  $('#p-views').wrap('<a href="http://www.wikipedia.org"></a>');
+
+  // Smooth scroll
+  $('#toc a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 75
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
