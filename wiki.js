117daf5296766395fa268aec08bc02f179e6cfb6
Lillian Chen <qc53@cornell.edu>
2015-06-23 17:49:46 -0400

    navbar get its toc

--- wiki.js
+++ wiki.js
@@ -2,8 +2,43 @@ $( document ).ready(function() {
   // Wiki logo link
   $('#p-views').wrap('<a href="http://www.wikipedia.org"></a>');
 
+  // TOC setup
+  var menu = $('#right-navigation #p-views');
+  var menuClone = $('#right-navigation #p-views')
+      .clone()
+      .addClass('override no-display')
+      .prependTo('.mediawiki #mw-navigation #mw-head #right-navigation');
+      
+  var toc = $('.toc'),
+      cT = toc.offset().top,
+      cH = toc.outerHeight();
+  var tocClone = $('.toc')
+      .clone()
+      .addClass('override no-display')
+      .prependTo('.mediawiki #content #bodyContent #mw-content-text');
+
+  var tocFixed = $('.toc.override'),
+      menuBars = $('#p-views.override');
+
+  // Toggle menu bars
+  function toggleMenu() {
+    var y_scroll_pos = window.pageYOffset;
+
+    if (y_scroll_pos > (cT+cH)) {
+      menu.addClass('no-display');
+      menuBars.removeClass('no-display');
+    } else {
+      menu.removeClass('no-display');
+      menuBars.addClass('no-display');
+      tocFixed.addClass('no-display');
+    }
+  }
+  toggleMenu();
+
   // Smooth scroll
-  $('#toc a').click(function() {
+  $('.toc a').click(function() {
+    tocFixed.addClass('no-display');
+    
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
       var target = $(this.hash);
@@ -16,4 +51,19 @@ $( document ).ready(function() {
       }
     }
   });
+
+  // Scroll behavior
+  $(window).on('scroll', function() {
+    toggleMenu();
+  });
+
+  // Toggle TOC
+  menuBars.click(function() {
+    tocFixed.toggleClass('no-display');
+    return false;
+  });
 });
+
+
+
+
