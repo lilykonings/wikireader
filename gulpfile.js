c1bb9470e2528c843650ae882d743ecf1689f21f
Lily Chen <lilyc5459@gmail.com>
2016-12-02 10:29:44 -0500

    bad commit, I know. folder restructure, add navbar fix from banner promo

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,53 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-  gulp.src('scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css/'));
+var cache           = require('gulp-cache');
+var clean           = require('gulp-clean');
+var concat          = require('gulp-concat');
+var imagemin        = require('gulp-imagemin');
+var jshint          = require('gulp-jshint');
+var minifycss       = require('gulp-minify-css');
+var plumber         = require('gulp-plumber');
+var rename          = require('gulp-rename');
+var sass            = require('gulp-sass');
+var stylish         = require('jshint-stylish');
+var uglify          = require('gulp-uglify');
+var declare         = require('gulp-declare');
+var shell           = require('gulp-shell');
+
+gulp.task('styles', function(){
+  return gulp.src('src/scss/*.scss')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('scripts', function(){
+  return gulp.src('src/js/*.js')
+    .pipe(plumber({
+      errorHandler: function (err) {
+        console.log(err);
+        this.emit('end');
+      }
+    }))
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
 //Watch task
-gulp.task('default',function() {
-  gulp.watch('scss/**/*.scss',['styles']);
+gulp.task('watch', function(){
+  gulp.watch('src/scss/*.scss', ['styles']);
+  gulp.watch('src/js/*.js', ['scripts']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'watch'], function(){
+  console.log('waiting...');
 });
---
+++ src/js/wiki.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+  // Wiki logo link
+  $('#p-views').wrap('<a href="http://www.wikipedia.org"></a>');
+
+  // TOC setup
+  var menu = $('#right-navigation #p-views');
+  var menuClone = $('#right-navigation #p-views')
+      .clone()
+      .addClass('override no-display')
+      .prependTo('.mediawiki #mw-navigation #mw-head #right-navigation');
+      
+  var toc = $('.toc'),
+      cT = toc.offset().top,
+      cH = toc.outerHeight();
+  var tocClone = $('.toc')
+      .clone()
+      .addClass('override no-display')
+      .prependTo('.mediawiki #content #bodyContent #mw-content-text');
+
+  var tocFixed = $('.toc.override'),
+      menuBars = $('#p-views.override');
+
+  var navbar = $('#mw-head'),
+      navbarTop = navbar.css('top').replace(/[^-\d\.]/g, '');
+
+  // Toggle menu bars
+  function toggleMenu() {
+    var yScrollPos = window.pageYOffset;
+
+    if (yScrollPos > (cT+cH)) {
+      menu.addClass('no-display');
+      menuBars.removeClass('no-display');
+    } else {
+      menu.removeClass('no-display');
+      menuBars.addClass('no-display');
+      tocFixed.addClass('no-display');
+    }
+
+    if (yScrollPos > navbarTop) {
+      navbar.addClass('sticky');
+    } else {
+      navbar.removeClass('sticky');
+    }
+  }
+  toggleMenu();
+
+  // Smooth scroll
+  $('.toc a').click(function() {
+    tocFixed.addClass('no-display');
+    
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 75
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  // Scroll behavior
+  $(window).on('scroll', function() {
+    toggleMenu();
+  });
+
+  // Toggle TOC
+  menuBars.click(function() {
+    tocFixed.toggleClass('no-display');
+    return false;
+  });
+});
+
